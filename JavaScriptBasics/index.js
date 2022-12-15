fe82d4fab0049a989fb33b2fdd3828c1f77d819d
Mohit Soni <mohit.soni@debutinfotech.com>
2022-12-15 10:39:38 +0530

    js complete

--- JavaScriptBasics/index.js
+++ JavaScriptBasics/index.js
@@ -55,3 +55,74 @@ const personDetail = {
   }
 
   let text = myFunction.toString();
+  class Car {
+    constructor(carName, year) {
+      this.carName = carName;
+      this.year = year;
+    }
+    age() {
+      let date = new Date();
+      return date.getFullYear() - this.year;
+    }
+  }
+
+  let myCar = new Car("Ford", 2014);
+  console.log("My car is " + myCar.age() + " years old.");
+  class Car1 {
+    constructor(brand) {
+      this.carname = brand;
+    }
+    present() {
+      return 'I have a ' + this.carname;
+    }
+  }
+  class Model extends Car1 {
+    constructor(brand, mod) {
+      super(brand);
+      this.model = mod;
+    }
+    show() {
+      return this.present() + ', it is a ' + this.model;
+    }
+  }
+
+  let myCar1 = new Model("Ford", "Mustang");
+  console.log(myCar1.show());
+
+  const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+
+// Call removeNeg with a callback
+const posNumbers = removeNeg(myNumbers, (x) => x >= 0);
+
+// Display Result
+console.log(posNumbers);
+// Keep only positive numbers
+function removeNeg(numbers, callback) {
+  const myArray = [];
+  for (const x of numbers) {
+    if (callback(x)) {
+      myArray.push(x);
+    }
+  }
+  return myArray;
+}
+
+function myDisplayer(value) {
+  console.log(value);
+}
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 0;
+
+// The producing code (this may take some time)
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
