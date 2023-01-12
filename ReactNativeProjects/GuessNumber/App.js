e755ff9fde305d44e2179e3f305787e2c66ed187
Mohit Soni <mohit.soni@debutinfotech.com>
2023-01-12 15:43:13 +0530

    project init

---
+++ ReactNativeProjects/GuessNumber/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  return (
+    <View>
+      <Text>Guess The Number</Text>
+    </View>
+  );
+};
+
+export default App;
