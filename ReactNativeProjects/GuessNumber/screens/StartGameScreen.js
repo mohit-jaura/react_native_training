ac73c8ac9413a84f05ab972dcfeeabeb3a39a6c3
Mohit Soni <mohit.soni@debutinfotech.com>
2023-01-23 17:54:11 +0530

    designing textFields and buttons

--- ReactNativeProjects/GuessNumber/screens/StartGameScreen.js
+++ ReactNativeProjects/GuessNumber/screens/StartGameScreen.js
@@ -5,9 +5,21 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.textField} />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.textField}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton style={styles.buttonContainer}>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -17,9 +29,11 @@ export default StartGameScreen;
 const styles = StyleSheet.create({
   inputContainer: {
     marginHorizontal: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: '#72064c',
     borderRadius: 14,
     elevation: 6,
     shadowColor: 'black',
@@ -30,8 +44,22 @@ const styles = StyleSheet.create({
 
   textField: {
     height: 50,
+    width: 50,
     fontSize: 32,
     borderBottomColor: '#ddb52f',
+    borderBottomWidth: 1,
     borderRadius: 10,
+    color: '#ddb52f',
+    paddingVertical: 3,
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
