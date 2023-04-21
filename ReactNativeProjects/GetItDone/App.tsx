534449da9bd88b0c9f2d6e2e0a0500c7cbdce34a
Mohit Soni <mosoni909@gmail.com>
2023-04-21 16:17:11 +0530

    todo list project created

---
+++ ReactNativeProjects/GetItDone/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Running on android phone</Text>
+    </View>
+  );
+}
+
+export default App;
